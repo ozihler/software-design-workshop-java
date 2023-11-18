4bf50713f3c7f022ae5831984b6c09f5c5361dd1
Oliver Zihler <ozihler@gmail.com>
2023-11-18 11:14:59 +0100

    fix ex4 branch

--- src/main/java/refactoring/Circle.java
+++ src/main/java/refactoring/Circle.java
@@ -2,65 +2,78 @@ package refactoring;
 
 
 public class Circle extends Shape {
-	private int x;
-	private int y;
-	private int r;
-	private Color color = new Color("Green");
+    private int x;
+    private int y;
+    private int r;
+    private Color color = new Color("Green");
 
-	public Circle(int x, int y, int r) {
-		if (r <= 0) {
-			throw new RuntimeException("Radius needs to be larger 0");
-		}
-		this.x = x;
-		this.y = y;
-		this.r = r;
-	}
+    public Circle(int x, int y, int r) {
+        if (r <= 0) {
+            throw new RuntimeException("Radius needs to be larger 0");
+        }
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
 
-	public int countContainedPoints(int[] xCords, int[] yCords) {
-		int numberOfContainedPoints = 0;
-		if (xCords == null || xCords.length == 0) {
-			throw new RuntimeException("x coordinates are empty");
-		}
-		if (yCords == null || yCords.length == 0) {
-			throw new RuntimeException("y coordinates are empty");
-		}
-		if (xCords.length != yCords.length) {
-			throw new RuntimeException("Not every provided x coordinate has a matching y coordinate");
-		}
-		for (int i = 0; i < xCords.length; ++i) {
-			if (contains(xCords, yCords, i)) {
-				numberOfContainedPoints++;
-			}
-		}
-		return numberOfContainedPoints;
-	}
+    public int countContainedPoints(int[] xCords, int[] yCords) {
+        validateCoordinates(xCords, yCords);
 
-	public boolean contains(int[] xCords, int[] yCords, int i) {
-		var deltaX = xCords[i] - this.x;
-		var deltaY = yCords[i] - this.y;
-		return square(deltaX) + square(deltaY) <= square(r);
-	}
+        int numberOfContainedPoints = 0;
+        for (int i = 0; i < xCords.length; ++i) {
+            if (contains(xCords, yCords, i)) {
+                numberOfContainedPoints++;
+            }
+        }
+        return numberOfContainedPoints;
+    }
 
-	private int square(int value) {
-		return value * value;
-	}
+    private static void validateCoordinates(int[] xCords, int[] yCords) {
+        if (isEmpty(xCords)) {
+            throw new RuntimeException("x coordinates are empty");
+        }
+        if (isEmpty(yCords)) {
+            throw new RuntimeException("y coordinates are empty");
+        }
+        if (differInLength(xCords, yCords)) {
+            throw new RuntimeException("Not every provided x coordinate has a matching y coordinate");
+        }
+    }
 
-	public void moveTo(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    private static boolean differInLength(int[] xCords, int[] yCords) {
+        return xCords.length != yCords.length;
+    }
 
-	public void resize(int r) {
-		this.r = r;
-	}
+    private static boolean isEmpty(int[] coordinates) {
+        return coordinates == null || coordinates.length == 0;
+    }
 
-	@Override
-	public String format() {
-		return "circle: {" +
-				"\n\tcenter: (" + this.x + "," + this.y + ") " +
-				"\n\tradius: " + this.r +
-				"\n\tcolor: " + this.color.getColorFormatted(false)
-				+ "\n}";
-	}
+    public boolean contains(int[] xCords, int[] yCords, int i) {
+        var deltaX = xCords[i] - this.x;
+        var deltaY = yCords[i] - this.y;
+        return square(deltaX) + square(deltaY) <= square(r);
+    }
+
+    private int square(int value) {
+        return value * value;
+    }
+
+    public void moveTo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void resize(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public String format() {
+        return "circle: {" +
+                "\n\tcenter: (" + this.x + "," + this.y + ") " +
+                "\n\tradius: " + this.r +
+                "\n\tcolor: " + this.color.getColorFormatted(false)
+                + "\n}";
+    }
 
 }
