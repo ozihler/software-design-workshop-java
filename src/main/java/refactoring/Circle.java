fefeab28cadcbb0ce9865a3e7f95be7b1f313545
Oliver Zihler <ozihler@gmail.com>
2023-11-18 11:16:37 +0100

    fix ex5 branch

--- src/main/java/refactoring/Circle.java
+++ src/main/java/refactoring/Circle.java
@@ -2,59 +2,73 @@ package refactoring;
 
 
 public class Circle extends Shape {
-	private Point center;
-	private Radius radius;
-	private Color color = new Color("Green");
+    private Point center;
+    private Radius radius;
+    private Color color = new Color("Green");
 
-	public Circle(Point center, Radius radius) {
-		this.center = center;
-		this.radius = radius;
-	}
+    public Circle(Point center, Radius radius) {
+        this.center = center;
+        this.radius = radius;
+    }
 
-	public int countContainedPoints(int[] xCords, int[] yCords) {
-		int numberOfContainedPoints = 0;
-		if (xCords == null || xCords.length == 0) {
-			throw new RuntimeException("x coordinates are empty");
-		}
-		if (yCords == null || yCords.length == 0) {
-			throw new RuntimeException("y coordinates are empty");
-		}
-		if (xCords.length != yCords.length) {
-			throw new RuntimeException("Not every provided x coordinate has a matching y coordinate");
-		}
-		for (int i = 0; i < xCords.length; ++i) {
-			if (contains(new Point(xCords[i], yCords[i]))) {
-				numberOfContainedPoints++;
-			}
-		}
-		return numberOfContainedPoints;
-	}
+    public int countContainedPoints(int[] xCords, int[] yCords) {
+        validateCoordinates(xCords, yCords);
 
-	public boolean contains(Point point) {
-		var deltaX = point.x() - this.center.x();
-		var deltaY = point.y() - this.center.y();
-		return square(deltaX) + square(deltaY) <= square(radius.value());
-	}
+        int numberOfContainedPoints = 0;
+        for (int i = 0; i < xCords.length; ++i) {
+            if (contains(new Point(xCords[i], yCords[i]))) {
+                numberOfContainedPoints++;
+            }
+        }
+        return numberOfContainedPoints;
+    }
 
-	private int square(int value) {
-		return value * value;
-	}
+    private static void validateCoordinates(int[] xCords, int[] yCords) {
+        if (isEmpty(xCords)) {
+            throw new RuntimeException("x coordinates are empty");
+        }
+        if (isEmpty(yCords)) {
+            throw new RuntimeException("y coordinates are empty");
+        }
+        if (differInLength(xCords, yCords)) {
+            throw new RuntimeException("Not every provided x coordinate has a matching y coordinate");
+        }
+    }
 
-	public void moveTo(Point newCenter) {
-		this.center = newCenter;
-	}
+    private static boolean differInLength(int[] xCords, int[] yCords) {
+        return xCords.length != yCords.length;
+    }
 
-	public void resize(Radius newRadius) {
-		this.radius = newRadius;
-	}
+    private static boolean isEmpty(int[] coordinates) {
+        return coordinates == null || coordinates.length == 0;
+    }
 
-	@Override
-	public String format() {
-		return "circle: {" +
-				"\n\tcenter: (" + this.center.x() + "," + this.center.y() + ") " +
-				"\n\tradius: " + this.radius.value() +
-				"\n\tcolor: " + this.color.getColorFormatted(false)
-				+ "\n}";
-	}
+
+    public boolean contains(Point point) {
+        var deltaX = point.x() - this.center.x();
+        var deltaY = point.y() - this.center.y();
+        return square(deltaX) + square(deltaY) <= square(radius.value());
+    }
+
+    private int square(int value) {
+        return value * value;
+    }
+
+    public void moveTo(Point newCenter) {
+        this.center = newCenter;
+    }
+
+    public void resize(Radius newRadius) {
+        this.radius = newRadius;
+    }
+
+    @Override
+    public String format() {
+        return "circle: {" +
+                "\n\tcenter: (" + this.center.x() + "," + this.center.y() + ") " +
+                "\n\tradius: " + this.radius.value() +
+                "\n\tcolor: " + this.color.getColorFormatted(false)
+                + "\n}";
+    }
 
 }
