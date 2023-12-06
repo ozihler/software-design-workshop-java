082ca95fb0c6317438d23d3456cfbfc64493ee23
Oliver Zihler <oliver.zihler1@swisscom.com>
2023-12-06 13:44:27 +0100

    remove imports

--- src/main/java/refactoring/Point.java
+++ src/main/java/refactoring/Point.java
@@ -1,13 +1,16 @@
 package refactoring;
 
-public record Point(int x, int y) {
-    public String format() {
-        return "(" + x() + "," + y() + ")";
-    }
+import static java.lang.Math.sqrt;
+import static refactoring.Maths.square;
 
-    public double distanceTo(Point point) {
-        var deltaX = point.x() - this.x();
-        var deltaY = point.y() - this.y();
-        return Math.sqrt(Maths.square(deltaX) + Maths.square(deltaY));
-    }
+public record Point(int x, int y) {
+	public String format() {
+		return "(" + x() + "," + y() + ")";
+	}
+
+	public double distanceTo(Point point) {
+		var deltaX = point.x() - this.x();
+		var deltaY = point.y() - this.y();
+		return sqrt(square(deltaX) + square(deltaY));
+	}
 }
